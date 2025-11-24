00f0c5d847af968ff46e9c58d66703a65245ec8a
Larster <149896479+Larster05@users.noreply.github.com>
2025-11-24 21:14:45 +0100

    added Step 9: REST API

--- netbox_sitemap/api/serializers.py
+++ netbox_sitemap/api/serializers.py
@@ -1,3 +1,5 @@
+from dcim.api.serializers import SiteGroupSerializer, SiteSerializer
+
 from rest_framework import serializers
 
 from netbox.api.serializers import NetBoxModelSerializer
@@ -7,10 +9,15 @@ class SitemapSerializer(NetBoxModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='plugins-api:netbox_sitemap-api:sitemap-detail'
     )
+    site_groups = SiteGroupSerializer(nested=True, required=False, allow_null=True)
+    sites = SiteSerializer(nested=True, required=False, allow_null=True)
 
     class Meta:
         model = Sitemap
         fields = (
-            'id', 'display', 'name', 'site_groups', 'sites', 'comments', 'tags', 'custom_fields', 'created',
-            'last_updated',
+            'id', 'url', 'display', 'name', 'site_groups', 'sites', 'comments', 'tags', 'custom_fields', 'created',
+            'last_updated'
+        )
+        brief_fields =(
+            'id', 'url', 'display', 'name', 'site_groups', 'sites'
         )
